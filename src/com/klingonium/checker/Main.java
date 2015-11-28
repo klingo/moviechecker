1cc7197d71507b3b20f02370da5ea68cac5f99eb
klingo <klingo@klingonium.com>
2015-11-28 13:20:35 +0100

    added commons-lang jar added sub-packages

---
+++ src/com/klingonium/checker/Main.java
@@ -0,0 +1,16 @@
+package com.klingonium.checker;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+	public void doSomething() {
+		if (StringUtils.isEmpty("")) {
+			System.out.println("test");
+		}
+	}
+}
